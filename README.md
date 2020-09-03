5474fb79ba3cfa6ce21dfe6af28b5a84d680a6f1
mjannino <michaeljannino@gmail.com>
2020-09-02 21:50:16 -0400

    Bootstrap SQLite3 DB with Knex migration

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tu-challenge
 Coding challenge for CRUD application.
+
+1. `npm install`
+2. Create a .env file from the .env.example file.
+3. `npm run start`
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-import express from 'express'
-import bodyParser from 'body-parser'
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const port = process.env.port
+const port = process.env.port || 5000
 let app = express()
 
-app.use(bodyParser)
+app.use(bodyParser.json())
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`)
+})
+
+app.get("/health", (req, res) =>{
+    res.json({message: "Application tu-challenge healthy and running."})
 })
