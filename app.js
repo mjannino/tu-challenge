84353eb9008ced40bb0be3927861b89fcad77c70
mjannino <michaeljannino@gmail.com>
2020-09-04 16:29:34 -0400

    Optimize heroku deploy

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.listen(port, ()=>{
     console.log(`Listening on port ${port}`)
 })
 
+app.get('/', (req, res) => {
+    res.send("Welcome to the tu-challenge application. Endpoints are under the /api path.")
+})
+
 app.get("/health", (req, res) =>{
     res.json({message: "Application healthy and running."})
 })
